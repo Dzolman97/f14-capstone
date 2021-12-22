b9ca2c0d88fbcf2d50cf82847d45bb315922f34c
Daniel Zolman <zolman.daniel@gmail.com>
2021-12-22 11:46:11 -0700

    update commit

--- seed.js
+++ seed.js
@@ -21,12 +21,16 @@ module.exports = {
 
       create table cc_users (
          user_id SERIAL PRIMARY KEY,
+         phone_num  VARCHAR(10)
          full_name VARCHAR(300),
          user_name VARCHAR(100),
          password VARCHAR(1000)
       );
 
-
+         insert into cc_users (phone_num, full_name, user_name, password)
+         values ('1234567891', 'Daniel Zolman', 'DZolman97', 'Dwzphzmi6'),
+         ('1234567891', 'Tester McTesting', 'Test1', 'testing123');
+         
       `)
    }
 }
